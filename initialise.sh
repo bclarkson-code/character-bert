62e7774b87e53b4a90482b1418f64abdfdf2e71f
bclarkson-code <57139598+bclarkson-code@users.noreply.github.com>
2021-07-28 00:38:37 +0100

    Added init file

---
+++ initialise.sh
@@ -0,0 +1,5 @@
+python3 download.py --model='general_character_bert'
+gsutil cp gs://search-query-classification-us-central1-c/character_model.py .
+gsutil cp gs://search-query-classification-us-central1-c/run_model.py .
+gsutil cp gs://search-query-classification-us-central1-c/open_source.feather .
+cp ~/run.sh .
